f2009b8ff03361a14d13e5ce73afbefb227c939a
Katrine Brandt Forsmo <katrinebforsmo@gmail.com>
2024-11-18 00:35:43 +0000

    Updated sd2-db.sql to improve Fish Pie description in Menu table

--- sd2-db.sql
+++ sd2-db.sql
@@ -64,3 +64,11 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- Update the description for 'Fish Pie'
+UPDATE Menu
+SET Description = 'Fresh tuna in creamy white sauce, topped with mashed potatoes and baked golden.'
+WHERE Name = 'Fish Pie';
+
+
+
