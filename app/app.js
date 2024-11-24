66fe38717b1c57d501b1161b609e91b58371904b
Katrine Brandt Forsmo <katrinebforsmo@gmail.com>
2024-11-24 20:27:21 +0000

    Removed redundant lines

--- app/app.js
+++ app/app.js
@@ -55,16 +55,6 @@ app.get("/restaurants", function(req, res) {
     res.render("restaurant_profile");
 });
 
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
-});
 
 // Start server on port 3000
 app.listen(3000,function(){
