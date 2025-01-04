fa27bf099a72f9d03701b29dd2c3725d856f1170
👨🏽‍💻Dev <53455765+eniola07@users.noreply.github.com>
2025-01-04 21:05:52 +0000

    hashing and salting added

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,8 @@
 // Get the functions in the db.js file to use
 const { match } = require("assert");
 const db = require("../services/db");
-
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs'); // or use bcryptjs
+const saltRounds = 10; // Defines the complexity of the salt
 
 class User {
   // Id of the user
@@ -27,6 +27,33 @@ class User {
       return false;
     }
   }
+  async hashPassword(password) {
+    try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        console.log('Hashed Password:', hashedPassword);
+        return hashedPassword; // You can save this to your database
+    } catch (error) {
+        console.error('Error hashing password:', error);
+    }
+  }
+
+  async  verifyPassword(enteredPassword, storedHash) {
+    try {
+        const isMatch = await bcrypt.compare(enteredPassword, storedHash);
+        if (isMatch) {
+            console.log('Password is correct');
+            return true 
+            // Allow user to log in
+        } else {
+            console.log('Password is incorrect');
+            return false
+            // Deny login
+        }
+    } catch (error) {
+        console.error('Error verifying password:', error);
+    }
+}
+
 
   // Add a password to an existing user
   async setUserPassword(password) {
@@ -36,12 +63,13 @@ class User {
     return true;
   }
 
+  
   // Add a new record to the users table
   async addUser(name, email, password, phone) {
-    const pw = await bcrypt.hash(password, 10);
+    const pw = await this.hashPassword(password);
     var sql =
       "INSERT INTO Users (name, email, password, MoblieNumber) VALUES (? , ? , ? , ?)";
-    const result = await db.query(sql, [name, email, password, phone]);
+    const result = await db.query(sql, [name, email, pw, phone]);
     console.log(result.insertId);
     this.id = result.insertId;
     return true;
@@ -53,9 +81,10 @@ class User {
     var sql = "SELECT password FROM Users WHERE id = ?";
     const result = await db.query(sql, [uId]);
     let passmatch = false;
+    
 
     // const match = await bcrypt.compare(password, result[0].password);
-    if (password === result[0].password) {
+    if (await this.verifyPassword(password, result[0].password)) {
       passmatch = true;
     } else {
       passmatch = false;
@@ -67,6 +96,7 @@ class User {
       return false;
     }
   }
+  
 }
 
 module.exports = {
